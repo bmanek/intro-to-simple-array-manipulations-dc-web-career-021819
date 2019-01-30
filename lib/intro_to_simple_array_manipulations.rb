de97eda56ed689059fb86be714cc741fc6505c4a
Ben Manekin <ben.manekin@gmail.com>
2019-01-30 17:25:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,14 +29,18 @@ end
 
   
 def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
   
+def using_insert(array, new_element)
+  array.insert(new_element, 4)
+end
+
+
+
   
-  
-  
-  
-  
-  
-  
+
   
   
   
